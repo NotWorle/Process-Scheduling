3806ccb24cc0c26e7550692f8949deac50a26629
Lê Đức Huy <duchuy.notworle@gmail.com>
2025-06-09 20:44:08 +0700

    cập nhật

--- OperatingSystem.py
+++ OperatingSystem.py
@@ -1,32 +1,5 @@
 from collections import deque
-
-"""
-Multiple parts
--   The program code, also called text section
--   Current activity including program counter, processor registers
--   Stack containing temporary data Function parameters, return addresses, local variables
--   Data section containing global variables
--   Heap containing memory dynamically allocated during run time
-"""
-class Process: #tiến trình
-    def __init__(self,text_section, stack:deque, data, heap):
-        self.text_section = text_section
-        self.stack = stack
-        self.data = data
-        self.heap = heap
-        self.state = "new"
-
-    def program_count(self):
-        pass
-
-    def process_register(self):
-        pass
-
-    def change(self, state):
-        state_space = ["new", "running", "waiting", 'ready', 'terminated']
-
-        if state in state_space:
-            self.state = state
+import heapq
 
 class ProgramCounter:
     def __init__(self, initial_address:list):
@@ -48,13 +21,48 @@ class ProgramCounter:
         self.index = index
 
 
-
-
+"""Process Control Block be created when execute context switch, it contains address of information each process"""
 class ProcessControlBlock:
-    def __init__(self, process:Process,):
+    def __init__(self, pc:ProgramCounter, pr, memory, accounting, io):
+        self.state = "new"
+        self.pc = pc                      # Program counter
+        self.pr = pr                      # Processor registers
+        self.memory = memory              # chứa địa chỉ text, stack, data, heap
+        self.accounting = accounting      # CPU time, priority,...
+        self.io = io                      # I/O devices
+
+    def change_state(self, state):
+        if state in ["new", "running", "waiting", 'ready', 'terminated']:
+            self.state = state
+
+"""
+Multiple parts
+-   The program code, also called text section
+-   Current activity including program counter, processor registers
+-   Stack containing temporary data Function parameters, return addresses, local variables
+-   Data section containing global variables
+-   Heap containing memory dynamically allocated during run time
+Process contains just contain 
+"""
+wait = deque()
+ready = deque()
+class Process:
+    def __init__(self, text_section, stack: deque, data, heap, pcb: ProcessControlBlock):
+        self.text_section = text_section
+        self.stack = stack
+        self.data = data
+        self.heap = heap
+        self.pcb = pcb   # Mỗi tiến trình có 1 PCB đi kèm
+
+    def add_program(self,program):
         pass
+
+    @staticmethod
+    def load_process(pcb:ProcessControlBlock):
+        ready.append(pcb)
+
 if __name__ == "__main__":
-    PC1 = ProgramCounter(["A=3","b=2",'print()','a = fun(c)'])
-    print(PC1.get_address())
-    PC1.jump(2)
-    print(PC1.get_address())
+    # PC1 = ProgramCounter(["A=3","b=2",'print()','a = fun(c)'])
+    # print(PC1.get_address())
+    # PC1.jump(2)
+    # print(PC1.get_address())
